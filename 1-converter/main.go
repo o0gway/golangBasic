e5ab61b0bdc33ddc4a4c96ff3f5f735cbcefa52b
Wenzel <kachman87@gmail.com>
2025-12-24 15:35:50 +0300

    Add two funcs

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,9 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	const USDToEUR = 0.85
 	const RUBToUSD = 79.88
 	const EURToRub = 79.88 / 0.85
+
+}
+
+func getCurrency(currency float64) float64 {
+	fmt.Scan(&currency)
+	return currency
+}
+
+func getRate(currency float64, rate1 float64, rate2 float64) {
 
 }
