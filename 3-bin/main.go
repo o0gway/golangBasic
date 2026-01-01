43a191197e9c292b7bcd5f4dca4e1c447c4b632c
Wenzel <kachman87@gmail.com>
2026-01-01 19:08:51 +0300

    Add new app 3-bin

---
+++ 3-bin/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "time"
+
+type Bin struct{
+	id string
+	private bool
+	createdAt time.Time
+	name string
+}
+
+func NewBin(id string, private bool, name string) *Bin {
+	return &Bin{
+		id: id,
+		private: private,
+		createdAt: time.Now(),
+		name: name,
+	}
+}
+
+func main() {
+
+	Bins := []Bin{}
+
+
+}
